2377ab5ca55dbe04578d9974cfc9916a6793316d
Ignasi Fosch <natx@y10k.ws>
2015-03-28 13:27:12 +0100

    Solutions proposed for hider and seeker

--- hider.py
+++ hider.py
@@ -1,7 +1,58 @@
 #!/usr/bin/env python
+import socket
+import random
 import argparse
+import zmq
+from zmq.eventloop import ioloop, zmqstream
+
+city = ''
+cities = open('cities.txt').readlines()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', default='5556')
 parser.add_argument('-b', '--broker', default='tcp://127.0.0.1:5555')
-args = parser.parse_args()
+
+def get_ip():
+    return socket.gethostbyname(socket.gethostname())
+
+def register(port):
+    context = zmq.Context()
+    zmq_socket = context.socket(zmq.DEALER)
+    zmq_socket.connect(args.broker)
+    zmq_socket.send('REGISTER {}:{}'.format(get_ip(), port))
+    reply = zmq_socket.recv()
+    if reply == 'HOP':
+        port = register(str(int(port) + 1))
+    zmq_socket.close()
+    return port
+
+def choose_city():
+    return random.choice(cities).strip()
+
+def handle(stream, message):
+    global city
+    addr, text = message
+    print('HIDER: ' + text)
+    if text == city:
+        print('HIDER: Found!')
+        reply = 'CORRECT'
+        city = choose_city()
+        print 'HIDER: Chosen', city
+    else:
+        reply = 'INCORRECT'
+    stream.send_multipart((addr, reply))
+
+io_loop = ioloop.IOLoop()
+context = zmq.Context()
+zmq_socket = context.socket(zmq.ROUTER)
+stream = zmqstream.ZMQStream(zmq_socket, io_loop=io_loop)
+stream.on_recv_stream(handle)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    port = register(args.port)
+    print 'HIDER: Using', get_ip(), port
+    city = choose_city()
+    print 'HIDER: Chosen', city
+    zmq_socket.bind('tcp://{}:{}'.format(get_ip(), port))
+    io_loop.start()
--- seeker.py
+++ seeker.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python
 import argparse
+import random
+import zmq
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-b', '--broker', default='tcp://127.0.0.1:5555')
+
+cities = open('cities.txt').readlines()
+
+def get_hiders():
+    context = zmq.Context()
+    zmq_socket = context.socket(zmq.DEALER)
+    zmq_socket.connect(args.broker)
+    zmq_socket.send('LIST')
+    return zmq_socket.recv().split()
+
+def choose_city():
+    return random.choice(cities).strip()
+
+def guess(hider):
+    context = zmq.Context()
+    zmq_socket = context.socket(zmq.DEALER)
+    print 'tcp://{}'.format(hider.strip())
+    zmq_socket.connect('tcp://{}'.format(hider.strip()))
+    city = choose_city()
+    print 'SEEKER: Chosen', city
+    zmq_socket.send(city)
+    reply = zmq_socket.recv()
+    if reply == 'CORRECT':
+        print 'SEEKER: Found {} in {}'.format(hider, city)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    hiders = get_hiders()
+    print 'SEEKER: ', hiders
+    for hider in hiders:
+        guess(hider)
